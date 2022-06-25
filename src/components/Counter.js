50686aaffe727f86b5ce58bb7ebcc213b2d97c14
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-06-25 19:10:03 +0200

    ReduceTookit is used in place of Redux way

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,33 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
-  const toggleCounterHandler = () => {};
-
-  const incrementHandler = () => {
-    dispatch({ type: "increment" });
+  const toggle = useSelector((state) => state.showCounter);
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
   };
 
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
+  };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {toggle && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase By 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
