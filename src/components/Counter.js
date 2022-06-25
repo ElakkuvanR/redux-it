58768975937edd5beb45269fcfd7267d54cb63f8
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-06-25 20:54:16 +0200

    Login and Logout Functionalities are added with Redux and React-Redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,8 +4,8 @@ import { counterActions } from "../store/index";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const toggle = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const toggle = useSelector((state) => state.counter.showCounter);
   const toggleCounterHandler = () => {
     dispatch(counterActions.toggle());
   };
